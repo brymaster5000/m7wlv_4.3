6afd2191ce1f52f10877cb117a1d96edf3cad058
Bryan Maltais <brymaster5000@gmail.com>
2013-12-14 22:34:58 -0500

    revert over/under clocks

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -38,18 +38,6 @@
 #include "acpuclock-krait.h"
 #include "avs.h"
 
-#ifdef CONFIG_LOW_CPUCLOCKS
-#define FREQ_TABLE_SIZE      39
-#else
-#define FREQ_TABLE_SIZE      35
-#endif
-
-#ifdef CONFIG_CPU_OVERCLOCK
-#define OVERCLOCK_EXTRA_FREQS  7
-#else
-#define OVERCLOCK_EXTRA_FREQS  0
-#endif
-
 #define CPU_FOOT_PRINT_MAGIC				0xACBDFE00
 static void set_acpuclk_foot_print(unsigned cpu, unsigned state)
 {
@@ -952,7 +940,7 @@ void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
 #endif	/* CONFIG_CPU_VOTALGE_TABLE */
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
 
 static void __init cpufreq_table_init(void)
 {
